ab768b73d458d9004248b6b0b7ff0c590ed101bc
Kasper <kasper.janssens.android@gmail.com>
2015-05-09 18:25:04 +0200

    Renaming stuff

--- src/Renaming/ImportSomething.hs
+++ src/Renaming/ImportSomething.hs
@@ -1,6 +1,10 @@
 module Renaming.ImportSomething where
 
+-- Rename RenameMe to Spul, check cabal file.
 import Renaming.RenameMe
 
+--rename re to dingske
+
+-- rename reallyRenameMe to dongske, go ctrl b to declaration, then ctrl z
 useMe :: Int -> String -> Int -> String -> Int
 useMe re name me too = re + me + reallyRenameMe
